12e8d45fe581efe89f3b57a9ccae273eaf733a16
gabrielrodris <biel79082157@gmail.com>
2025-08-14 16:42:34 -0300

    refazer front-end

--- controle-financeiro/front-end/controle-financeiro-frontend/public/workbox-config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  globDirectory: 'public/',
-  globPatterns: [
-    '**/*.{html,json,js,css,png,jpg,jpeg,svg,gif,ico,webp,woff,woff2,ttf,eot}'
-  ],
-  swDest: 'public/sw.js',
-  clientsClaim: true,
-  skipWaiting: true
-};
